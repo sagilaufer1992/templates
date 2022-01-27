5f866159f4d1141b5006a9011c9c832cfcfe4809
Yaron Yarimi <yaronya@users.noreply.github.com>
2022-01-27 19:17:01 +0200

    Update cluster.tf

--- aws/k8s/cluster.tf
+++ aws/k8s/cluster.tf
@@ -1,5 +1,5 @@
 module "my-cluster" {
-  source = "github.com/env0/k8s-modules//aws"
+  source = "github.com/env0/k8s-modules/aws?ref=chore-replace-kubectl-provider"
 
   region       = var.region
   cluster_name = var.cluster_name
