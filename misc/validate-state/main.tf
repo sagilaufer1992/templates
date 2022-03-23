6437ea547479e94e3e4824eabf47d574e0542827
Sagi Laufer <sagi.laufer@env0.com>
2022-03-23 13:56:05 +0200

    one long change and another short

--- misc/validate-state/main.tf
+++ misc/validate-state/main.tf
@@ -14,6 +14,6 @@ resource "null_resource" "null3" {
 
 resource "null_resource" "null2" {
   triggers = {
-    ids = join(",",[null_resource.null1.id,null_resource.null3.id])
+    ids = join(",",[null_resource.null1.id])
   }
 }
