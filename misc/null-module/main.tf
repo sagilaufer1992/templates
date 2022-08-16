022f0b43a2575d069e38af2cf69e2d2fa31a1ec1
Sagi Laufer <sagi.laufer@env0.com>
2022-08-16 15:02:52 +0300

    other module

--- misc/null-module/main.tf
+++ misc/null-module/main.tf
@@ -1,4 +1,4 @@
-module "raz-module2444" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/raz-module2444/raz"
+module "sagi-module" {
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/sagi-module/gh"
   version = "v1.0.0"
 }
