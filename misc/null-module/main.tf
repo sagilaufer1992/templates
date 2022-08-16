0d5a3558d92e2c0df1236e209ee19e8c32c32d39
Sagi Laufer <sagi.laufer@env0.com>
2022-08-16 14:27:13 +0300

    check another module

--- misc/null-module/main.tf
+++ misc/null-module/main.tf
@@ -1,4 +1,4 @@
-module "private_gh_module" {
-  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/private_gh_module/avner"
+module "raz-module2444" {
+  source = "api-dev.dev.env0.com/bd12a04a-b051-4f51-9a8a-8a32ac198802/raz-module2444/raz"
   version = "v1.0.0"
 }
