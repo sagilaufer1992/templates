3ae4bbce2666bf942100c64babdd43188cfd2a97
Sagi Laufer <sagi.laufer@env0.com>
2023-12-18 15:33:14 +0200

    different approach

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -20,28 +20,6 @@ provider "tfe" {}
 # root folder terraform
 resource "null_resource" "null-tfc" {
 }
-data "tfe_workspace" "test" {
-  name         = "templates-old-tf"
-  organization = "sagi-env0"
-}
-
-data "tfe_variables" "vars" {
-  workspace_id = data.tfe_workspace.test.id
-}
-
-output "vars" {
-  value = {
-    name  = "vars"
-    value = data.tfe_variables.vars
-  }
-}
-
-output "ws" {
-  value = {
-    name  = "ws"
-    value = data.tfe_workspace.test.id
-  }
-}
 
 resource "tfe_variable" "sagi_test_secret" {
   category     = "env"
@@ -50,14 +28,11 @@ resource "tfe_variable" "sagi_test_secret" {
   key          = "sagi_secret_2"
   sensitive    = true
   workspace_id = "ws-FBW3jexDrsf43tmS"
-  value = "laufer_secret_2"
+  value        = "laufer_secret_2"
 }
 
-
-output "sagi_test_import" {
-  sensitive = true
-  value     = {
-    name  = "sagi_test_import"
-    value = tfe_variable.sagi_test_secret.value
+resource "null_resource" "print_stuff" {
+  provisioner "local-exec" {
+    command = "echo $sagi_secret"
   }
 }
