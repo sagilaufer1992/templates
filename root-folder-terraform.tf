0ff858eeda8f0a1f737562d2bdb792954ead8c73
Sagi Laufer <sagi.laufer@env0.com>
2023-12-18 14:10:47 +0200

    add sensitive output

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -40,3 +40,20 @@ output "ws" {
     value = data.tfe_workspace.test.id
   }
 }
+
+resource "tfe_variable" "sagi_test_import" {
+    workspace_id = data.tfe_workspace.test.id
+    key          = "sagi_secret"
+    category     = "env"
+    sensitive    = true
+    hcl          = false
+}
+
+
+output "sagi_test_import" {
+  sensitive = true
+  value = {
+    name  = "sagi_test_import"
+    value = tfe_variable.sagi_test_import.value
+  }
+}
