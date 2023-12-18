ed58e93b0da1780504e3a77989f821e644c4aeba
Sagi Laufer <sagi.laufer@env0.com>
2023-12-18 12:38:15 +0200

    add stuff for tf

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -1,3 +1,42 @@
-# root folder terraform 
+terraform {
+  cloud {
+    organization = "sagi-env0"
+
+    workspaces {
+      name = "templates-old-tf"
+    }
+  }
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.50.0"
+    }
+  }
+}
+
+# root folder terraform
 resource "null_resource" "null" {
+}
+data "tfe_workspace" "test" {
+  name         = "templates-old-tf"
+  organization = "sagi-env0"
+}
+
+data "tfe_variables" "vars" {
+  workspace_id = data.tfe_workspace.test.id
+}
+
+output "vars" {
+  value = {
+    name  = "vars"
+    value = data.tfe_variables.vars
+  }
+}
+
+output "ws" {
+  value = {
+    name  = "ws"
+    value = data.tfe_workspace.test.id
+  }
 }
