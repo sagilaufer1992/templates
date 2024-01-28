fb0694691c1bc336f99ab5110bd47e84af03eb75
sagilaufer1992 <sagi.laufer23@gmail.com>
2024-01-28 17:07:12 +0200

    Update root-folder-terraform.tf

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -1,8 +1,28 @@
-# root folder terraform 
+  # root folder terraform 
 resource "null_resource" "null" {
-  count = 5000
+  count = 3000
 }
 
-output "null_output" {
+output "null_output1" {
+  value = null_resource.null[*]
+}
+
+output "null_output2" {
+  value = null_resource.null[*]
+}
+
+output "null_output3" {
+  value = null_resource.null[*]
+}
+
+output "null_output4" {
+  value = null_resource.null[*]
+}
+
+output "null_output5" {
+  value = null_resource.null[*]
+}
+
+output "null_output6" {
   value = null_resource.null[*]
 }
