53243fa74de2a6fe8446cb588202b3bc28aa6e1d
sagilaufer1992 <sagi.laufer23@gmail.com>
2024-01-28 14:10:09 +0200

    Update root-folder-terraform.tf

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -2,3 +2,8 @@
 resource "null_resource" "null" {
   count = 1000
 }
+
+resource "null_resource" "null-dep" {
+  count = 500
+  depends_on = [null_resource.null[count.index], null_resource.null[2 * count.index + 1]]
+}
