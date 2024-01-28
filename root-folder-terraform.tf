ad508f1c41007819f98f7eb9492cb823ff806777
sagilaufer1992 <sagi.laufer23@gmail.com>
2024-01-28 17:13:00 +0200

    Update root-folder-terraform.tf

--- root-folder-terraform.tf
+++ root-folder-terraform.tf
@@ -26,3 +26,75 @@ output "null_output5" {
 output "null_output6" {
   value = null_resource.null[*]
 }
+
+output "null_output7" {
+  value = null_resource.null[*]
+}
+
+output "null_output8" {
+  value = null_resource.null[*]
+}
+
+output "null_output9" {
+  value = null_resource.null[*]
+}
+
+output "null_output10" {
+  value = null_resource.null[*]
+}
+
+output "null_output11" {
+  value = null_resource.null[*]
+}
+
+output "null_output12" {
+  value = null_resource.null[*]
+}
+
+output "null_output13" {
+  value = null_resource.null[*]
+}
+
+output "null_output14" {
+  value = null_resource.null[*]
+}
+
+output "null_output15" {
+  value = null_resource.null[*]
+}
+
+output "null_output16" {
+  value = null_resource.null[*]
+}
+
+output "null_output17" {
+  value = null_resource.null[*]
+}
+
+output "null_output18" {
+  value = null_resource.null[*]
+}
+
+output "null_output19" {
+  value = null_resource.null[*]
+}
+
+output "null_output20" {
+  value = null_resource.null[*]
+}
+
+output "null_output21" {
+  value = null_resource.null[*]
+}
+
+output "null_output22" {
+  value = null_resource.null[*]
+}
+
+output "null_output23" {
+  value = null_resource.null[*]
+}
+
+output "null_output24" {
+  value = null_resource.null[*]
+}
